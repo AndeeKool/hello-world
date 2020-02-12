f41b9cc3b8f612b3a6a50c3bdf7e0ce68fb5dcb8
Andrea <andrea.antillon.b@gmail.com>
2020-02-12 08:46:05 -0700

    hello world implemented

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _00_Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
